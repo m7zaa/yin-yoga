86ad6a00bde94fcf2217ec2ae84168fa2013ac8c
Micah Zaayer <micahzaayer@gmail.com>
2019-12-10 10:09:38 -0800

    add header and empty components.

---
+++ constants/colors.js
@@ -0,0 +1,6 @@
+export default {
+  primary: '#ffe7d1',
+  accent1: '#f6c89f',
+  accent2: '#4b8e8d',
+  accent3: "#396362"
+}
---
+++ screens/CreateFlow.js
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
