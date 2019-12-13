ef3d449e62d4d1121f4e3ba23c99426fd15b129e
Micah Zaayer <micahzaayer@gmail.com>
2019-12-13 11:55:16 -0800

    fix errors in poses.json and add nextPose function.

--- screens/Routine.js
+++ screens/Routine.js
@@ -10,76 +10,49 @@ import BodyText from '../components/BodyText';
 
 const Routine = props => {
   const [randomRoutine, setRandomRoutine] = useState([]);
-  // const [randomNumber, setRandomNumber] = useState([]);
   console.log(randomRoutine)
 
-//   const generateRandomPoseHandler = () => {
-//         const poseArray = [];
-//         for(let i = 0; i < 6; i++) {
-//           let randomNumber = Math.floor(Math.random() * 10) + 0;
-//           poseArray.push(poses[randomNumber])
-//           numArray.push(randomNumber)
-//           setRandomRoutine(poseArray)
-//         };
-//       }
-
-
-
-const generateRandomPoseHandler = () => {
-  const poseArray = [];
-  const numArray = [];
-
-        for(let i = 0; i < 6; i++) {
-            let randomNum = Math.floor(Math.random() * 10) + 0;
-              if(numArray.includes(randomNum)) {
-                return generateRandomPoseHandler();
-              } else{
-                poseArray.push(poses[randomNum])
-                numArray.push(randomNum)
-                setRandomRoutine(poseArray)
-              }
+  const generateRandomPoseHandler = () => {
+    const poseArray = [];
+    const numArray = [];
+    for(let i = 0; i < 6; i++) {
+        let randomNum = Math.floor(Math.random() * 10) + 0;
+          if(numArray.includes(randomNum)) {
+            return generateRandomPoseHandler();
+          } else{
+            poseArray.push(poses[randomNum])
+            numArray.push(randomNum)
+            setRandomRoutine(poseArray)
           }
       }
+  };
 
 
-
-
-  // const generateRandomNumber = () => {
-  //   let randomNum = Math.floor(Math.random() * 10) + 0;
-  //   if(randomNumber.includes(randomNum)) {
-  //     return generateRandomNumber();
-  //   } else{
-  //     setRandomNumber([randomNum]);
-  //   }
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+  const poseCycleHandler = (poseIndex) => {
+    poseIndex= (poseIndex+1);
+    return poseIndex;
+  };
 
   let routineOutput;
+  
+  let poseIndex=0;
+
   if (randomRoutine.length > 5) {
     routineOutput = (
       <View>
         <Card style={styles.poseContainer}>
-          <TitleText style={styles.title}>{randomRoutine[0].english_name} Pose</TitleText>
+          <TitleText style={styles.title}>{randomRoutine[poseIndex].english_name} Pose</TitleText>
           <Image
             style={styles.image}
-            source={{ uri: randomRoutine[0].img_url }}
+            source={{ uri: randomRoutine[poseIndex].img_url }}
             resizeMode='contain'
           /> 
           <Button title="Next" 
-          // onPress={} 
+          onPress={() => {
+            let s= poseIndex
+            s++
+            poseIndex=s
+          }} 
           />
         </Card>
       </View>
